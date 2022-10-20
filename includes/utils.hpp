24ffd347b175aaa43f6051af8ff9c51fcbb73098
Alexander SABOURET <asaboure@e1r6p16.clusters.42paris.fr>
2022-10-20 14:40:01 +0200

    added canread function

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:52:42 by asaboure          #+#    #+#             */
-/*   Updated: 2022/10/18 17:05:51 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/10/20 13:42:17 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int     ft_stoi(const std::string &);
 long    ft_stol(const std::string &);
 char    *ft_strdupcpp(const char *);
 int     isDirectory(const std::string &);
+bool    canRead(const std::string &);
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:50:57 by asaboure          #+#    #+#             */
-/*   Updated: 2022/10/18 17:05:36 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/10/20 13:47:52 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 #include <fstream>
 #include <sstream>
 #include <sys/stat.h>
+#include <unistd.h>
+
+bool canRead(const std::string &name){
+    return !access(name.c_str(), R_OK);
+}
 
 bool existsFile (const std::string& name) {
-    std::ifstream f(name.c_str());
-    return f.good();
+    return !access(name.c_str(), F_OK);
 }
 
 int isDirectory(const std::string &path) {
