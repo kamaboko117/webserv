65da7a9a34e58fffdbcb0cea7c6385d4d5772a74
Alexander SABOURET <asaboure@e1r6p16.clusters.42paris.fr>
2022-10-20 14:39:10 +0200

    autoindex is now a list

--- autoindex.php
+++ autoindex.php
@@ -2,8 +2,10 @@
 $path    = $_GET["PATH"];
 $files = scandir($path);
 $files = array_diff(scandir($path), array('.', '..'));
+echo "<ul>";
+// echo "<li><a href='/$path/..'>..</a></li>";
 foreach($files as $file){
-  echo "<a href='$path/$file'>$file</a>";
-  echo "<P>";
+  echo "<li><a href='/$path/$file'>$file</a></li>";
 }
+echo "<ul>";
 ?>
